a83b580519dcf899fc76773ee3f357caeb247fc0
nourallahahmedrunprof <124608217+Nourallah-ahmed@users.noreply.github.com>
2023-06-08 21:17:22 +0300

    Initial Commit

---
+++ Product_Viewer/Product_ViewerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Product_ViewerApp.swift
+//  Product_Viewer
+//
+//  Created by NourAllah on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Product_ViewerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
